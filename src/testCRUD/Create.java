d38bdc7428d1481805f2ab120f242a2dedd2458f
shushan8816 <shmatevosyatl@gmail.com>
2021-10-17 21:39:00 +0400

    update

--- src/testCRUD/Create.java
+++ src/testCRUD/Create.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Create {
+
     static Scanner inputCommand = new Scanner(System.in);
     public static String currentFileName;
 
